1264552d5d334001208e4f0aed2a7b3f86c61072
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-07-30 15:16:20 +0300

    breakout.js

--- breakout.js
+++ breakout.js
@@ -120,13 +120,13 @@ function getRandomNeonColor() {
 
 function drawScore() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#FFF"; // Beyaz skor
+    ctx.fillStyle = "#FFF"; 
     ctx.fillText("Score: " + score, 8, 20);
 }
 
 function drawLives() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#FFF"; // Beyaz canlar
+    ctx.fillStyle = "#FFF"; 
     ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
 }
 
